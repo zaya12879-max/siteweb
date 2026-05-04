5917533dd9d95973c68b4ae33eee063b0a08f9e9
zaya12879-max <zaya12879@gmail.com>
2026-05-04 20:05:17 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/adsdo-agency/', // Hada huwa l-koud lli kiy-fekk l-mouchkil
+  base: '/siteweb/', 
 })
